bd6dcc4780c83cc106aa14490ac20d553d2926e7
Deyvison Nogueira <deyvison_s@hotmail.com>
2019-06-11 22:42:17 -0300

    inicio experimentos

---
+++ scrypt.py
@@ -0,0 +1,16 @@
+
+import os
+
+instances = ['100','200','300','400','500','600','700','800','900','1000','1100',]
+variacao = ['TS_DIVERSIFICATION_INFLUENCE','TS_INFLUENCE','TS_SURROGATE_INFLUENCE']
+seed = ['10','15','20','25','30',]
+
+
+for v in variacao:			
+	for x in instances:
+		for s in seed:
+			print "Iniciou a instacia " + x + " com a variacao "+v + "com o k = " +s
+			os.system("java -jar "+v+".jar  " +x +" "+ s + " > "+v+"/instance"+x+"-"+s+".txt")
+			print "Terminou a instancia " + x +"com o k = " +s
+		
+print("FIM")
